184a175f57e5516a9786f785926aa438a3fb18b2
Julian NG <juliannieb@gmail.com>
2016-09-21 14:03:58 -0500

    Add bfs cpp file

---
+++ bfs.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+typedef stack<char> sc;
+typedef vector<sc> vsc;
+
+int main () {
+	
+	return 0;
+}
