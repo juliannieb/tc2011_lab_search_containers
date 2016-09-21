91a5705c950528e0c0809394fff385ec66238b10
Julian NG <juliannieb@gmail.com>
2016-09-21 14:34:10 -0500

    Add node structure for tree

--- bfs.cpp
+++ bfs.cpp
@@ -8,6 +8,12 @@ using namespace std;
 typedef stack<char> sc;
 typedef vector<sc> vsc;
 
+struct Node {
+	struct Node *parent;
+	vsc state;
+	pair<int, int> movement;
+};
+
 int main () {
 	
 	return 0;
